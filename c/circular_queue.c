cdc026ba68b2a0e84933791c0c5d28406a37e6fc
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2020-07-23 22:45:33 +0900

    C language

--- c/circular_queue.c
+++ c/circular_queue.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define MAX 20
+#define MAX 5
 
 typedef int element;
-typedef struct queue{
+typedef struct {
 	element data[MAX];
-	int front;
-	int rear;
+	int front,rear;
 }Queue;
 
 void error(char*message){
@@ -19,39 +18,46 @@ void init(Queue*q){
 }
 
 int is_full(Queue*q){
-	return q->rear==MAX-1;
+	return (q->rear+1)%MAX==q->front;
 }
 
 int is_empty(Queue*q){
-	return q->rear==q->front;
+	return q->front==q->rear;
 }
 
 void enqueue(Queue*q,element item){
 	if(is_full(q))
-		error("큐 포화에러");
-	else
-		q->data[++q->rear]=item;
+		error("큐 포화 에러\n");
+	
+	q->rear=(q->rear+1)%MAX;
+	q->data[q->rear]=item;
 }
 
 element dequeue(Queue*q){
 	if(is_empty(q))
-		error("큐 공백에러");
-	else
-		return q->data[++q->front];
+		error("큐 공백 에러\n");
+	
+	q->front=(q->front+1)%MAX;
+	return q->data[q->front];
 }
 
 int main (void){
 	Queue*q=malloc(sizeof(Queue));
 	init(q);
-	enqueue(q,10); enqueue(q,20); enqueue(q,30);
-	
+	for(int i=1;i<5;i++)
+		enqueue(q,i*10);
+	//enqueue(q,50); //큐 포화
 	while(!is_empty(q))
 		printf("dequeue %d\n",dequeue(q));
+	
+	for(int i=1;i<5;i++)
+		enqueue(q,i*10);
 
+
+	while(!is_empty(q))
+		printf("dequeue %d\n",dequeue(q));
+	
 	free(q);
 	return 0;
 }
-
-
-
 
